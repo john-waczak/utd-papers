8b41d828373881de9e0dfc940d19b2a6751ecc21
John Waczak <john.louis.waczak@gmail.com>
2023-07-16 11:44:52 -0500

    adding submission for download job

---
+++ TimeSeriesMethods1/submit_0.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#SBATCH     --job-name=0__download-data
+#SBATCH     --output=0__download-data.out
+#SBATCH     --error=0__download-data.err
+#SBATCH     --ntasks=64
+#SBATCH     --cpus-per-task=1  # number of threads for multi-threading
+#SBATCH     --mem-per-cpu=2G
+#SBATCH     --time=1-00:00:00  # 2 day max
+#SBATCH     --partition=normal
+#SBATCH     --mail-type=ALL
+#SBATCH     --mail-user=jxw190004@utdallas.edu
+
+
+julia --project=. 0__download-data.jl
